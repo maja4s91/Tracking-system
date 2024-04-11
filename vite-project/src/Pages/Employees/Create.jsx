67b1344ba18810374472c0fa73559c830f7e2275
maja4s91 <majastojanovic4@gmail.com>
2024-04-11 20:12:55 +0200

    Form layout fix

--- vite-project/src/Pages/Employees/Create.jsx
+++ vite-project/src/Pages/Employees/Create.jsx
@@ -49,32 +49,32 @@ const Create=()=>{
            <form className="row g-3  mt-md-5 mt-sm-2 ">
                 <div className="d-flex justify-content-end mb-md-5 mb-sm-0 "><button type="button" className="btn-close px-sm-2" aria-label="Close" onClick={handleClose}></button></div> 
 
-                <div className="col-md-6">
+                <div className="col-md-12">
                    <label htmlFor="full-name" className="form-label fs-2">Full name</label>
                    <input onChange={handleTypo}  name="fullName" type="text" id="full-name" className="form-control fs-2 mb-md-5 mb-sm-2" placeholder="Jovan Jovanovic"/>
                 </div>
           
-                <div className="col-md-6">
+                <div className="col-md-5">
                   <label htmlFor="emailId" className="form-label fs-2">Email</label>
                   <input onChange={handleTypo}  name="email" type="email" id="emailId" className="form-control fs-2 mb-md-5 mb-sm-2 input-border-color" placeholder="jovan4@gmail.com"/>
                 </div>
 
-                <div className="col-md-5">
+                <div className="col-md-4">
                   <label htmlFor="phoneNumberId" className="form-label fs-2">Phone number</label>
                   <input onChange={handleTypo}  name="phoneNumber" type="text" id="phoneNumberId" className="form-control fs-2 mb-md-5 mb-sm-2" placeholder="+381647961696"/>
                 </div>
 
-                <div className="col-md-4">
+                <div className="col-md-3">
                   <label htmlFor="birthDayId" className="form-label fs-2">Date of birth</label>
                   <input onChange={handleTypo}  name="birthDay" type="date" id="birthDayId" className="form-control fs-2 mb-md-5 mb-sm-2" placeholder="23/1/1996"/>
                 </div>
 
-                <div className="col-md-3">
+                <div className="col-md-2">
                   <label htmlFor="salaryId" className="form-label fs-2">Salary</label>
                   <input onChange={handleTypo}  name="salary" type="text" id="salaryId" className="form-control fs-2 mb-md-5 mb-sm-2" placeholder="800e"/>
                 </div>
 
-                {isActive && (<div className="col-12 mx-auto"><button onClick={handleSubmit} type="submit " className="btn btn-success fs-2 " >Submit</button></div>)}
+                {isActive && (<div className="d-flex justify-content-center"><button onClick={handleSubmit} type="submit " className="btn btn-success fs-2 " >Submit</button></div>)}
  
              </form>
         </div>
