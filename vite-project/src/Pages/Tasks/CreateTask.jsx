0d3bf8150f4a9e674632465e75eed5a824f9b39c
maja4s91 <majastojanovic4@gmail.com>
2024-04-17 15:36:47 +0200

    New tasks form added

--- vite-project/src/Pages/Tasks/CreateTask.jsx
+++ vite-project/src/Pages/Tasks/CreateTask.jsx
@@ -50,117 +50,129 @@ const CreateTask = () => {
   };
 
   return (
-    <form className="form-create-task">
-      <button
-        type="button"
-        className="btn-close"
-        aria-label="Close"
-        onClick={() => {
-          nav("/tasks");
-        }}
-      ></button>
+    <div className="container-sm mt-md-5 mt-sm-2  ">
+      <form className="row g-3  mt-md-5 mt-sm-2 ">
+        <div className="d-flex justify-content-end mb-md-5 mb-sm-0 ">
+          <button
+            type="button"
+            className="btn-close px-sm-2"
+            aria-label="Close"
+            onClick={() => nav("/tasks")}
+          ></button>
+        </div>
 
-      <div className="input-tas">
-        <label htmlFor="title">Title</label>
-        <input
-          onChange={handleTypo}
-          className="title"
-          type="text"
-          placeholder="Fix gaps"
-          required
-          name="title"
-        />
-      </div>
-
-      <div className="input-tas">
-        <label htmlFor="description">Description</label>
-        <textarea
-          onChange={handleTypo}
-          className="description"
-          rows="6"
-          // maxlength="3000"
-          placeholder=""
-          required
-          name="description"
-        ></textarea>
-      </div>
-
-      {/* <input
-            className="full-name-task"
+        <div className="col-md-6">
+          <label htmlFor="title" className="form-label fs-2">
+            Title
+          </label>
+          <input
+            onChange={handleTypo}
+            name="title"
             type="text"
-            placeholder="Maja Djordjevic"
+            id="title"
+            className="form-control fs-2 mb-md-5 mb-sm-2"
+            placeholder="Fix gaps"
+          />
+        </div>
+
+        <div className="col-md-6">
+          <label htmlFor="textareaId" className="form-label">
+            Description
+          </label>
+          <textarea
+            onChange={handleTypo}
+            name="description"
+            className="form-control"
+            id="textareaId"
+            rows="5"
+            placeholder=""
+          ></textarea>
+        </div>
+
+        <div className="col-md-6">
+          <label htmlFor="assigneeId" className="form-label fs-2">
+            Assignee
+          </label>
+          <select
+            onChange={handleTypo}
+            name="assignee"
+            className="form-select"
+            aria-label="Default select example"
+            id="assigneeId"
             required
-          /> */}
-      <div className="input-tas">
-        <label htmlFor="full-name-task">Assignee</label>
-        <select
-          onChange={handleTypo}
-          id="select-assignee"
-          className="full-name-task"
-          required
-          name="assignee"
-        >
-          <option value="">Choose asignee</option>
-          {employeesArr.map((e) => (
-            <option value={e.employee.fullName}>{e.employee.fullName}</option>
-          ))}
-        </select>
-      </div>
-      <div className="input-tas">
-        <label htmlFor="status-task">Task status</label>
-        <select
-          onChange={handleTypo}
-          id="select-status"
-          className="select-status"
-          name="taskStatus"
-          required
-        >
-          <option value="">Choose status</option>
-          <option value="To do">To do</option>
-          <option value="In progress">In progress</option>
+          >
+            <option selected>Choose asignee</option>
+            {employeesArr.map((e) => (
+              <option value={e.employee.fullName}>{e.employee.fullName}</option>
+            ))}
+          </select>
+        </div>
 
-          <option value="Completed">Completed</option>
-        </select>
-      </div>
+        <div className="col-md-6">
+          <label htmlFor="select-statusId" className="form-label fs-2">
+            Task status{" "}
+          </label>
+          <select
+            onChange={handleTypo}
+            name="taskStatus"
+            className="form-select"
+            aria-label="Default select example"
+            id="select-statusId"
+            required
+          >
+            <option selected>Choose status</option>
+            <option value="To do">To do</option>
+            <option value="In progress">In progress</option>
+            <option value="Completed">Completed</option>
+          </select>
+        </div>
 
-      <div className="input-tas">
-        <label htmlFor="task-epic">Epic</label>
-        <select
-          onChange={handleTypo}
-          id="select-epic"
-          className="task-epic"
-          name="epic"
-          required
-        >
-          <option value="">Unassigned</option>
+        <div className="col-md-6">
+          <label htmlFor="epicId" className="form-label fs-2">
+            Epic
+          </label>
+          <select
+            onChange={handleTypo}
+            name="epic"
+            className="form-select"
+            aria-label="Default select example"
+            id="epicId"
+            required
+          >
+            <option selected>Unassigned</option>
+            {epicArr.map((e) => (
+              <option value={e.title}>{e.title}</option>
+            ))}
+          </select>
+        </div>
 
-          {epicArr.map((e) => (
-            <option value={e.title}>{e.title}</option>
-          ))}
-        </select>
-      </div>
+        <div className="col-md-6">
+          <label htmlFor="dueId" className="form-label fs-2">
+            Due date
+          </label>
+          <input
+            onChange={handleTypo}
+            name="dueDate"
+            type="date"
+            id="dueId"
+            className="form-control fs-2 mb-md-5 mb-sm-2"
+            placeholder="23/1/2024"
+          />
+        </div>
 
-      <div className="input-tas">
-        <label htmlFor="due">Due date</label>
-        <input
-          onChange={handleTypo}
-          className="due"
-          type="date"
-          placeholder="23/1/2024"
-          name="dueDate"
-          required
-        />
-      </div>
-
-      <button onClick={handleSubmit} type="submit " className="submit-task">
-        Submit
-      </button>
-      {!isActive && (
-        <button type="edit" className="btn-edit-task">
-          Edit
-        </button>
-      )}
-    </form>
+        {isActive && (
+          <div className="d-flex justify-content-center">
+            <button
+              onClick={handleSubmit}
+              type="submit "
+              className="btn btn-success fs-2 "
+            >
+              Submit
+            </button>
+          </div>
+        )}
+      </form>
+    </div>
   );
 };
 
