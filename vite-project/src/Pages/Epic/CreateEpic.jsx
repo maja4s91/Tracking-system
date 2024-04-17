90328558c20f19c2f4197decd6c64bbd907f4709
maja4s91 <majastojanovic4@gmail.com>
2024-04-17 16:44:31 +0200

    New epic form added

--- vite-project/src/Pages/Epic/CreateEpic.jsx
+++ vite-project/src/Pages/Epic/CreateEpic.jsx
@@ -36,69 +36,77 @@ const CreateEpic = () => {
   };
 
   return (
-    <form className="form-create-epic vh-80">
+    <div className="container-sm mt-md-5 mt-sm-2  ">
+      <form className="row g-3  mt-md-5 mt-sm-2 ">
+        <div className="d-flex justify-content-end mb-md-5 mb-sm-0 ">
+          <button
+            type="button"
+            className="btn-close px-sm-2"
+            aria-label="Close"
+            onClick={() => nav("/epic")}
+          ></button>
+        </div>
 
-      <button type="button" className="btn-close" aria-label="Close" onClick={() => {
-          nav("/epic");
-        }}></button>
-      <div className="input-epi">
-        <label htmlFor="title">Title</label>
-        <input
-          onChange={handleTypo}
-          className="title-epic"
-          type="text"
-          name="title"
-          placeholder="Fix gaps"
-          required
-        />
-      </div>
+        <div className="col-md-6">
+          <label htmlFor="title" className="form-label fs-2">
+            Title
+          </label>
+          <input
+            onChange={handleTypo}
+            name="title"
+            type="text"
+            id="title"
+            className="form-control fs-2 mb-md-5 mb-sm-2"
+            placeholder="Fix gaps"
+          />
+        </div>
 
-      <div className="input-epi">
-        <label htmlFor="status-epi">Task status</label>
-        <select
-          onChange={handleTypo}
-          id="select-status"
-          className="select-status-epic"
-          name="status"
-          required
-        >
-          <option value="">Choose status</option>
-          <option value="To do" className="epic-todo">
-            To do
-          </option>
-          <option value="In progress" className="epic-inprogress">
-            In progress
-          </option>
+        <div className="col-md-6">
+          <label htmlFor="select-statusId" className="form-label fs-2">
+            Task status{" "}
+          </label>
+          <select
+            onChange={handleTypo}
+            name="taskStatus"
+            className="form-select form-select-lg fs-2"
+            aria-label="Default select example"
+            id="select-statusId"
+            required
+          >
+            <option>Choose status</option>
+            <option value="To do">To do</option>
+            <option value="In progress">In progress</option>
+            <option value="Completed">Completed</option>
+          </select>
+        </div>
 
-          <option value="Completed" className="epic-completed">
-            Completed
-          </option>
-        </select>
-      </div>
+        <div className="col-md-12 mb-5 mb-sm-5">
+          <label htmlFor="textareaId" className="form-label fs-2">
+            Description
+          </label>
+          <textarea
+            onChange={handleTypo}
+            name="description"
+            className="form-control form-control-lg"
+            id="textareaId"
+            rows="5"
+            placeholder=""
+          ></textarea>
+        </div>
 
-      <div className="input-epi">
-        <label htmlFor="description">Description</label>
-        <textarea
-          onChange={handleTypo}
-          className="description-epic"
-          rows="6"
-          // maxlength="3000"
-          name="description"
-          placeholder=""
-          required
-        ></textarea>
-      </div>
-
-      {/* <div className="input-epi tasks-epic-box"></div> */}
-      <button onClick={handleSubmit} type="submit " className="submit-epic">
-        Submit
-      </button>
-      {!isActive && (
-        <button type="edit" className="btn-edit-epic">
-          Edit
-        </button>
-      )}
-    </form>
+        {isActive && (
+          <div className="d-flex justify-content-center">
+            <button
+              onClick={handleSubmit}
+              type="submit "
+              className="btn btn-success fs-2 "
+            >
+              Submit
+            </button>
+          </div>
+        )}
+      </form>
+    </div>
   );
 };
 
